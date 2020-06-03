d7affdc0e81a53e24c0302ca21f94a405d4232cc
mbade1 <mbade314@gmail.com>
2020-06-02 20:36:00 -0500

    added .each method

--- lib/blue_bell_final/blue_bell_ice_cream.rb
+++ lib/blue_bell_final/blue_bell_ice_cream.rb
@@ -1,9 +1,13 @@
+require 'open-uri'
+require 'nokogiri'
+
 class BlueBellFinal::BlueBellIceCream
 
   attr_accessor :name, :size, :description, :url
 
   def self.all
     #Scrape Blue Bell Ice Cream Site and return the name, size, description, and url on that data.
+    @@all
     self.scrape_flavors
   end
   
@@ -14,20 +18,24 @@ class BlueBellFinal::BlueBellIceCream
   end
 
   def self.scrape_blue_bell
-    doc = Nokogiri::HTML(open("https://www.bluebell.com/our-products/"))
-
-    flavor = self.new
-    flavor.name = doc.search("h4.description__title").text
-
-    #flavor = doc.css("div.products")
-    #flavor.size = doc.search("h4#description__title").text
-    #flavor.description = doc.search("h4#description__title h5").text
-    #flavor.url = doc.search("img#description__preview__image").attr("src")
-    flavor
-
-    end
-
+    site = Nokogiri::HTML(open('https://www.bluebell.com/our-products/'))
+  
+    @@all = []
+  
+    product = site.css("div.products .description.tabs .description__wrapper .description__inner")
+    product.each do |product_info|
+  
+      name = product_info.css(".description__aside h4.description__title").text
+      size = "half-gallon"
+      description = product_info.css(".description__body .description__content h5").text
+      nutrition = product_info.css(".description__aside ul.description__menu .nutrition_tabs .description__submenu .nutrition-item a.tab__trigger").attr("href").text
+  
+      product_hash = {:name => name, :size => size, :description => description, :nutrition => nutrition}
+      @@all << product_hash
+      end
+    @@all
   end
+end
 
 
 
